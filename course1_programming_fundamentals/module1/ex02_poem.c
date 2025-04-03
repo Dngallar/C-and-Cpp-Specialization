a765cd9c352c308515ffe6940dcff759a7003e03
Domingo Gallardo Saavedra <dngallar@uc.cl>
2025-04-03 20:41:03 -0300

    course 1 code update

---
+++ course1_programming_fundamentals/module1/ex02_poem.c
@@ -0,0 +1,27 @@
+/*
+    Poem
+    Domingo Gallardo Saavedra
+    April 4, 2025
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    printf("EL VIAJERO DE SÍ MISMO\n\n"
+           "Voy pisando cadáveres de amantes\n"
+           "y viejas tumbas llenas de pasado,\n"
+           "cubierto con cabello horripilante\n"
+           "del gran sepulcro universal tragado.\n\n"
+           "Acumulo mi yo exorbitante\n"
+           "y mi ilusión de Dios ensangrentado,\n"
+           "pues soy un espectáculo clamante\n"
+           "y un macho-santo ya desorbitado.\n\n"
+           "Mi amor te muerde como un perro de oro,\n"
+           "pero te exhibe en sus ancas de oro.\n"
+           "Winett, como una flor de extranjerí­a.\n\n"
+           "Porque sin ti no hubiera descubierto\n"
+           "como una jarra de agua en el desierto\n"
+           "la mina antigua de mi poesí­a.\n\n"
+           "- Pablo de Rokha");
+    return 0;
+}
---
+++ course1_programming_fundamentals/module2/ex02_sine_function.c
@@ -0,0 +1,26 @@
+/*
+    Sine Function for an input x between (0, 1)
+    Domingo Gallardo Saavedra
+    April 4, 2025
+*/
+
+// Includes
+#include <stdio.h>
+#include <math.h>
+
+// Main function
+int main(void) {
+    double value; // User input
+    double sine_calc; // Result
+    
+    // While loop until the user enter a range between (0, 1)
+    do {
+        printf("Enter a value between (0, 1): ");
+        scanf("%lf", &value);
+    } while (value <= 0.0 || value >= 1.0); // Condition
+
+    sine_calc = sin(value); // Sine with Math library
+    printf("The sine of %lf is %lf\n\n", value, sine_calc); // Print result
+
+    return 0; // Return 0 for success
+}
